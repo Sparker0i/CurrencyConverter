b5138fe6d2f9e440b19e55029f18fe298cb05316
Aaditya Menon <aaditya@sparker0i.me>
2018-10-20 14:35:17 +0530

    Configured Internet Connection Logic

--- app/src/main/java/me/sparker0i/mvvmsample/repo/CurrencyRepository.kt
+++ app/src/main/java/me/sparker0i/mvvmsample/repo/CurrencyRepository.kt
@@ -3,6 +3,7 @@ package me.sparker0i.mvvmsample.repo
 import android.os.HandlerThread
 import io.reactivex.Observable
 import me.sparker0i.mvvmsample.api.ApiInterface
+import me.sparker0i.mvvmsample.application.MvvmApplication
 import me.sparker0i.mvvmsample.database.RatesDao
 import me.sparker0i.mvvmsample.model.Rates
 import timber.log.Timber
@@ -17,6 +18,19 @@ class CurrencyRepository @Inject constructor(val apiInterface: ApiInterface,
         val observableFromApi = getCurrencyFromApi()
         val observableFromDb = getRatesFromDb()
         return Observable.concatArrayEager(observableFromApi , observableFromDb)
+    }
+
+    fun getCurrency(limit: Int, offset: Int): Observable<List<Rates>> {
+        val hasConnection = MvvmApplication.ifHasNetwork()
+        var observableFromApi: Observable<List<Rates>>? = null
+        if (hasConnection)
+            observableFromApi = getCurrencyFromApi()
+        val observableFromDb = getRatesFromDb()
+
+        return if (hasConnection)
+            Observable.concatArrayEager(observableFromApi , observableFromDb)
+        else
+            observableFromDb
     }
 
     private fun getCurrencyFromApi(): Observable<List<Rates>> {
