69c6707e37ca7e1b2c04fe550f674d3c878d10e6
Aaditya Menon <aaditya@sparker0i.me>
2018-10-20 18:58:48 +0530

    Loaded All Currencies

--- app/src/main/java/me/sparker0i/mvvmsample/ui/activity/MainActivity.kt
+++ app/src/main/java/me/sparker0i/mvvmsample/ui/activity/MainActivity.kt
@@ -2,9 +2,14 @@ package me.sparker0i.mvvmsample.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.android.AndroidInjection
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -17,6 +22,8 @@ import me.sparker0i.mvvmsample.api.ApiClient
 import me.sparker0i.mvvmsample.api.ApiInterface
 import me.sparker0i.mvvmsample.model.Currency
 import me.sparker0i.mvvmsample.model.Rates
+import me.sparker0i.mvvmsample.ui.adapter.InfiniteScrollListener
+import me.sparker0i.mvvmsample.ui.adapter.RateAdapter
 import me.sparker0i.mvvmsample.ui.model.CurrencyViewModel
 import me.sparker0i.mvvmsample.ui.model.CurrencyViewModelFactory
 import timber.log.Timber
@@ -26,30 +33,63 @@ class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var currencyViewModelFactory: CurrencyViewModelFactory
     lateinit var currencyViewModel: CurrencyViewModel
+    var rateAdapter = RateAdapter(ArrayList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         AndroidInjection.inject(this)
+
+        initializeRecyclerView()
+
         currencyViewModel = ViewModelProviders.of(this , currencyViewModelFactory)
                 .get(CurrencyViewModel::class.java)
-        currencyViewModel.getRates()
 
-        currencyViewModel.getCurrencyResult()
-                .observe(this , Observer<List<Rates>> {
-                    println(it)
-                    tv_hello_world.text = "Hello ${it.size} Currencies"
+        progressbar.visibility = View.VISIBLE
+        loadData()
+
+        currencyViewModel.getCurrencyResult().observe(this ,
+                Observer<List<Rates>> {
+                    if (it != null) {
+                        val position = rateAdapter.itemCount
+                        rateAdapter.addRates(it)
+                        recycler_view.adapter = rateAdapter
+                        recycler_view.scrollToPosition(position - 12)
+                    }
                 })
 
         currencyViewModel.getCurrencyError()
                 .observe(this , Observer<String> {
-                    tv_hello_world.text = "Hello Error ${it}"
+                    if (it != null) {
+                        Toast.makeText(this, "Could not load Currencies: " + it,
+                                Toast.LENGTH_SHORT).show()
+                    }
                 })
+
+        currencyViewModel.getCurrencyLoader().observe(this , Observer<Boolean> {
+            if (it == false)
+                progressbar.visibility = View.GONE
+        })
+
+        currencyViewModel.getRates()
     }
 
     override fun onDestroy() {
         currencyViewModel.disposeElements()
         super.onDestroy()
+    }
+
+    private fun initializeRecyclerView() {
+        val gridLayoutManager = GridLayoutManager(this , 1)
+        gridLayoutManager.orientation = RecyclerView.VERTICAL
+        recycler_view.apply {
+            setHasFixedSize(true)
+            layoutManager = gridLayoutManager
+            addOnScrollListener(InfiniteScrollListener({loadData()} , gridLayoutManager))
+        }
+    }
+
+    private fun loadData() {
+
     }
 }
